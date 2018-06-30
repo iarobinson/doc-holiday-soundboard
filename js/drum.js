0590b986e0e5ee4150587d217bb586a1bc27b918
Ian Robinson <ian@ianrobinson.net>
2018-06-30 00:29:28 -0700

    Final touch ups to grammar, clean up errors to console when user hits unassigned keys, remove FCC test suite. Interesting point: now that the keystrokes trigger audio, the tests do not pass.

--- js/drum.js
+++ js/drum.js
@@ -1,11 +1,24 @@
 window.onload = init;
 let displayEle, drumeles;
+
 function init() {
   displayEle = document.getElementById('display');
   drumEles = document.getElementsByClassName('drum-pad');
+  
+  // Thank you to Abhishek Hingorani for the StackOverflow info regarding vanilla JS
+  // https://stackoverflow.com/questions/6542413/bind-enter-key-to-specific-button-on-page
+  document.onkeydown = function (e) {
+    // List all keys that we want Doc to respond to
+    let boundKeys = "qweasdzxc".toUpperCase().split('');
+    let simpleKeystroke = e.key.toUpperCase();
+    if (boundKeys.indexOf(simpleKeystroke) >= 0) {
+      manageClick(document.getElementById(simpleKeystroke).parentElement);
+    }
+  }
 }
 
-function manageClick(ele) { 
+// This fuction manages the click based on what element is selected 
+function manageClick(ele) {
   displayEle.innerHTML = ele.parentElement.firstElementChild.innerHTML;
   ele.firstElementChild.play();  
 }
